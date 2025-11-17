6b9f2c09704d7ff779742c6723fc74bd459670e6
pursuitprimebc <bibhashchandra4850@gmail.com>
2025-11-17 20:49:42 +0530

    Delete week_1 directory

--- week_1/L1_6_1.py
+++
@@ -1,9 +0,0 @@
-
-n=str(input("type your name:"))
-
-p=str(input('type your location:'))
-
-w=str(input('hello',n,'how is the weather in',p,'?'))
-
-age=int(input("what is ur age?"))
-print("good to know u r",age,"years old")
--- week_1/L1_6_2.py
+++
@@ -1,5 +0,0 @@
-# variables can be used either side but literals cannot
-
-r = int(input('enter the radius of circle:'))
-area = 3.14 * r * r
-print('are of circle with radius', r , 'is',area)
