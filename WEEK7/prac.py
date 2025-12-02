fc527122dc474a2f0b329d2ead69edfde0eb5d5f
pursuitprimebc <bibhashchandra4850@gmail.com>
2025-12-02 19:16:47 +0530

    prac file

---
+++ WEEK7/prac.py
@@ -0,0 +1,14 @@
+
+n = int(input())
+
+i = 1
+# Update your code below this line
+while i<=n:
+    j=1
+    while j<=i:
+        for a in range(1,i):
+            print(a)
+            
+
+            
+    
