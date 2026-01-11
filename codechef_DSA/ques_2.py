864f7cb1833947c987a15b638f5b25cd3709e77b
pursuitprimebc <bibhashchandra4850@gmail.com>
2026-01-11 22:17:05 +0530

    newques

--- codechef_DSA/ques_2.py
+++ codechef_DSA/ques_2.py
@@ -1,1 +1,27 @@
-'''QUESTION - 
+'''QUESTION - Air Hockey
+Alice is playing Air Hockey with Bob. The first person to earn seven points wins the match. Currently, Alice's score is
+A and Bob's score is B.
+Charlie is eagerly waiting for his turn. Help Charlie by calculating the minimum number of points that will be further scored in the match before it ends.
+
+Input Format
+The first line of input will contain an integer T — the number of test cases. The description of 
+T test cases follows.The first and only line of each test case contains two space-separated integers A and 
+B, as described in the problem statement.
+
+Output Format
+For each test case, output on a new line the minimum number of points that will be further scored in the match before it ends.
+
+Constraints
+1≤T≤50
+0≤A,B≤6
+'''
+
+t = int(input())
+for i in range(t):
+    a , b = map(int, input().split())
+    maximum = max(a,b)
+    minimum_points_required = 7 - maximum
+    print(minimum_points_required)
+
+
+# EXPLANATION = 
