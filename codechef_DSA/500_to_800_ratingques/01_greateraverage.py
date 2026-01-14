85e4736130ce8727cd87adc2e31c16cf25a3fb98
pursuitprimebc <bibhashchandra4850@gmail.com>
2026-01-14 10:13:58 +0530

    newfolder

---
+++ codechef_DSA/500_to_800_ratingques/01_greateraverage.py
@@ -0,0 +1,17 @@
+''' QUESTION - Greater Average
+You are given 3 numbers A,B, and C.
+
+Determine whether the average of A and B is strictly greater than C or not?
+
+NOTE: Average of A and B is defined as (A+B)/2 . For example, average of 5 and 9 is 7, average of 5 and 8 is 6.5.
+
+Input Format
+The first line of input will contain a single integer T, denoting the number of test cases.
+Each test case consists of 3 integers A,B, and C.
+
+Output Format
+For each test case, output YES if average of A and B is strictly greater than C, NO otherwise.
+'''
+
+
+
