9d1778d0c5f00c4d10e7d85daaa20097b6da492c
pursuitprimebc <bibhashchandra4850@gmail.com>
2026-01-17 22:39:00 +0530

    valentine is coming

---
+++ codechef_beg_DSA/500_to_800_rating_problem/problem_52.py
@@ -0,0 +1,22 @@
+''' QUESTION -Valentine is Coming
+Valentine's Day is approaching and thus Chef wants to buy some chocolates for someone special.
+
+Chef has a total of X rupees and one chocolate costs Y rupees. What is the maximum number of chocolates Chef can buy?
+
+Input Format
+First line will contain T, the number of test cases. Then the test cases follow.
+Each test case contains a single line of input, two integers X,Y - the amount Chef has and the cost of one chocolate respectively.
+Output Format
+For each test case, output the maximum number of chocolates Chef can buy.
+'''
+
+
+t = int(input())
+for i in range(t):
+    x,y = map(int, input().split())
+    no_of_chocolates = x//y 
+    print(no_of_chocolates)
+    
+
+
+
