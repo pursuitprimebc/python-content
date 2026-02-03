7244c9f043bf4d6beda999f24745139a1b052a2e
pursuitprimebc <bibhashchandra4850@gmail.com>
2026-02-03 21:26:51 +0530

    Melt Gold

---
+++ codechef_beg_DSA/800_to_1000_rated_problems/problem_10.py
@@ -0,0 +1,31 @@
+''' QUESTION - Melt Gold
+Chef has an ore with melting point of X degrees
+Chef’s kiln has a initial temperature of Y degrees. The temperature of the kiln increases by i degrees after the ith minute.
+
+Find the minimum time in minutes after which the ore starts melting.
+
+Note:
+
+We are only concerned about the temperature at the end of each minute and not during a minute.
+The ore starts melting if the temperature of the kiln becomes greater than or equal to the melting point.
+Input Format
+The first line of input will contain a single integer T, denoting the number of test cases.
+Each test case consists of two space-separated integers X and Y — the melting point of the ore and the initial temperature of kiln.
+Output Format
+For each test case, output on a new line, the minimum time in minutes after which the ore starts melting.
+'''
+
+
+
+t = int(input())
+for i in range(t):
+    x,y = map(int, input().split())
+    minutes = 0
+    while y<x:
+        minutes += 1
+        y = y + minutes
+    
+    print(minutes)
+    
+
+
