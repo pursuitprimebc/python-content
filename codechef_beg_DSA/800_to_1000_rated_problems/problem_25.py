504daab336dba94dbbdae0a9d1f1fcafa64f991a
pursuitprimebc <bibhashchandra4850@gmail.com>
2026-02-04 21:16:05 +0530

    relativity

---
+++ codechef_beg_DSA/800_to_1000_rated_problems/problem_25.py
@@ -0,0 +1,24 @@
+''' QUESTION - Relativity
+Read problem statements in Bengali, Mandarin Chinese, Russian, and Vietnamese as well.
+In Chefland, the speed of light is c m/s, and acceleration due to gravity is g m/s2 .
+Find the smallest height (in meters) from which Chef should jump such that during his journey down only under the effect of gravity and
+independent of any airresistance, he achieves the speed of light and verifies Einstein's theory of special relativity.
+Assume he jumps at zero velocity and at any time, his velocity (v) and depth of descent (H) are related as v2 =2⋅g⋅H.
+
+Input
+The first line contains an integer T, the number of test cases. Then the test cases follow.
+Each test case contains a single line of input, two integers g,c.
+Output
+For each test case, output in a single line the answer to the problem. We can show that under the constraints, the answer is an integer. 
+'''
+
+
+
+t = int(input())
+for i in range(t):
+    g,c = map(int, input().split())
+    height = ((c**2)/(2*g))
+    print(int(height))
+    
+
+
