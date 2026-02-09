2a7f9be5f1a1f4a6c055c428bcf44e698fe7a351
pursuitprimebc <bibhashchandra4850@gmail.com>
2026-02-09 18:50:39 +0530

    ---------

---
+++ codechef_beg_DSA/800_to_1000_rated_problems/problem_42.py
@@ -0,0 +1,17 @@
+''' QUESTION - Two Ranges
+Chef has two ranges [A,B] and [C,D]. Chef needs to find the number of integers that belong to at least one of the ranges.
+
+Note: A range [P,Q] contains all the integers {P,P+1,P+2,…,Q-1,Q}.
+
+Input Format
+The first line of input will contain a single integer T, denoting the number of test cases.
+Each test case consists of a single line containing 4 integers A,B,C, and D, as mentioned in the statement.
+Output Format
+For each test case, output on a new line, the number of integers that belong to at least one of the ranges.
+'''
+
+
+
+
+
+
