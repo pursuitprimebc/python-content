2ce4f8d41e16566267367cd7d8b7affa3548f32b
pursuitprimebc <bibhashchandra4850@gmail.com>
2026-02-10 11:53:03 +0530

    Two vs Ten

---
+++ codechef_beg_DSA/800_to_1000_rated_problems/problem_49.py
@@ -0,0 +1,31 @@
+''' QUESTION - Two vs Ten
+Read problems statements in Mandarin chinese, Russian and Vietnamese as well.
+Chef Two and Chef Ten are playing a game with a number X. In one turn, they can multiply X by 2. The goal of the game is to make X divisible by 10.
+
+Help the Chefs find the smallest number of turns necessary to win the game (it may be possible to win in zero turns) or determine that it is impossible.
+
+Input
+The first line of the input contains a single integer T denoting the number of test cases. The description of T test cases follows.
+The first and only line of each test case contains a single integer denoting the initial value of X.
+Output
+For each test case, print a single line containing one integer — the minimum required number of turns or -1 if there is no way to win the game.
+'''
+
+
+
+t = int(input())
+for i in range(t):
+    x = int(input())
+    count = 0 
+    if x%10==0:
+        print('0')
+    elif x%5==0:
+        print('1')
+    else:
+        print('-1')
+    
+
+
+
+
+
