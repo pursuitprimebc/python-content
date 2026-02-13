158c5b6ecf309805db4d9d1c6b9c7fbbdde58e48
pursuitprimebc <bibhashchandra4850@gmail.com>
2026-02-13 20:18:11 +0530

    Farmer And His Plot [R]

---
+++ codechef_beg_DSA/practice_basic_math/problem_15.py
@@ -0,0 +1,26 @@
+''' QUESTION - Farmer And His Plot
+Santosh has a farm at Byteland. He has a very big family to look after. His life takes a sudden turn and he runs into a financial crisis. 
+After giving all the money he has in his hand, he decides to sell his plots. The speciality of his land is that it is rectangular in nature. 
+Santosh comes to know that he will get more money if he sells square shaped plots. So keeping this in mind, he decides to divide his land into 
+minimum possible number of square plots, such that each plot has the same area, and the plots divide the land perfectly. He does this in order to get the maximum profit out of this.
+
+So your task is to find the minimum number of square plots with the same area, that can be formed out of the rectangular land, such that they divide it perfectly.
+
+Input Format
+The first line of the input contains T, the number of test cases. Then T lines follow.
+The first and only line of each test case contains two space-separated integers, N and M, the length and the breadth of the land, respectively.
+Output Format
+For each test case, print the minimum number of square plots with equal area, such that they divide the farm land perfectly, in a new line.
+'''
+
+
+import math 
+t=int(input())
+for i in range(t):
+    n,m =map(int, input().split())
+    l_of_sqr = math.gcd(n,m)
+    area_of_sqr = l_of_sqr*l_of_sqr
+    area_of_rect = n*m 
+    no_of_sqr_plots = area_of_rect//area_of_sqr
+    print(no_of_sqr_plots)
+    
