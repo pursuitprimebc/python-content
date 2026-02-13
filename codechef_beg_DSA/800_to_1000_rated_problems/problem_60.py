fa7754d01567ae58b2955c1c211bc8ffa2ee2b39
pursuitprimebc <bibhashchandra4850@gmail.com>
2026-02-13 11:35:27 +0530

    qures

---
+++ codechef_beg_DSA/800_to_1000_rated_problems/problem_60.py
@@ -0,0 +1,19 @@
+''' QUESTION - Joining Date
+N candidates (numbered from 1 to N) join Chef's firm. The first 5 candidates join on the first day, and then, on every subsequent day, the next 5 candidates join in.
+For example, if there are 12 candidates, candidates numbered 1 to 5 will join on day 1, candidates numbered 6 to 10 on day 2 and the remaining 2 candidates will join on day 3.
+
+Candidate numbered K decided to turn down his offer and thus, Chef adjusts the position by shifting up all the higher numbered candidates. This leads to a change in 
+the joining day of some of the candidates.
+
+Help Chef determine the number of candidates who will join on a different day than expected.
+
+Input Format
+First line will contain T, number of test cases. Then the test cases follow.
+Each test case contains of a single line of input, two space-separated integers N and K denoting the number of candidates and the candidate who turned down the offer.
+Output Format
+For each test case, output a single integer denoting the number of candidates whose joining day will be changed.
+'''
+
+
+
+
