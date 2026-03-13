a7aff2ba8786e181e7cbadd2f0ff9c8531134283
pursuitprimebc <bibhashchandra4850@gmail.com>
2026-03-14 01:00:28 +0530

    wordle

---
+++ codechef_beg_DSA/practice_strings/problem_03.py
@@ -0,0 +1,33 @@
+''' QUESTION - Wordle
+Chef invented a modified wordle.
+
+There is a hidden word S and a guess word T, both of length 5.
+
+Chef defines a string M to determine the correctness of the guess word. For the ith index:
+
+If the guess at the ith index is correct, the ith  character of M is G.
+If the guess at the ith  index is wrong, the ith character of M is B.
+Given the hidden word S and guess T, determine string M.
+
+Input Format
+First line will contain T, number of test cases. Then the test cases follow.
+Each test case contains of two lines of input.
+First line contains the string S - the hidden word.
+Second line contains the string T - the guess word.
+Output Format
+For each test case, print the value of string M.
+'''
+
+
+a = int(input())
+for i in range(a):
+    s = input()
+    t = input()
+    m = ""
+    for i in range(len(s)):
+        if s[i]==t[i]:
+            m += "G"
+        else:
+            m += "B"
+    
+    print(m)
